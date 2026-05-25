89c3bcdc48307bcbbf0ce052284c7f6d64a62e0e
advit-kaluri <arjunkaluri@gmail.com>
2026-05-26 00:07:48 +0530

    Add files via upload

---
+++ number swap.py
@@ -0,0 +1,10 @@
+a = input("enter first number (a):")
+b = input("enter second number (b):")
+c = input("enter third number (c):")
+
+print(f'/nbefore swapping: a = {a}')
+
+print(f''after swpping: a = {a},b)
+
+
+
